02261901b24dedbf872b197c23d3ed4777aff16e
prnvshrn <prnvshrn@gmail.com>
2018-04-01 23:37:22 -0700

    Added image layout in Main Page

--- public/javascripts/main_menu_components.js
+++ public/javascripts/main_menu_components.js
@@ -1,6 +1,6 @@
 var background = {marginHeight:'10px'};
-
-
+var imageWidth = {width:'25%'};
+var imageParms = {width:'100%', height:'250px'};
 
 var HackPanels = React.createClass({
     render:function() {
@@ -15,11 +15,41 @@ var HackPanels = React.createClass({
 var Image = React.createClass({
     render:function () {
         return(
+            <div>
+            <div className="row">
+            <div className="col-sm-4">
             <div className="thumbnail">
-                <img src={books_image}/>
+                <img src={videogames_image} style={imageParms}/>
                 <div className="caption">
-                    <h2>Books</h2>
+                    <h2>Video Games
+                        <button type="button" className="btn btn-primary"><span className="badge">7</span> Listings</button>
+                    </h2>
                 </div>
+            </div>
+            </div>
+            <div className="col-sm-4">
+            <div className="thumbnail">
+                <img src={books_image} style={imageParms}/>
+                <div className="caption">
+                    <h2>Books
+                        <button type="button" className="btn btn-success"><span className="badge">7</span> Listings</button>
+                    </h2>
+                </div>
+            </div>
+            </div>
+
+                <div className="col-sm-4">
+                    <div className="thumbnail">
+                        <img src={appliance_image} style={imageParms}/>
+                        <div className="caption">
+                            <h2>Appliances
+                                <button type="button" className="btn btn-info"><span className="badge">7</span> Listings</button>
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
             </div>
         );
     }
