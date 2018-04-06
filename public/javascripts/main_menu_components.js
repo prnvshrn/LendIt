5ae2ebe21bb7eb576ed3de594535414cf88bd9fa
prnvshrn <prnvshrn@gmail.com>
2018-04-05 23:50:18 -0700

    Edited main page layout

--- public/javascripts/main_menu_components.js
+++ public/javascripts/main_menu_components.js
@@ -1,6 +1,24 @@
 var background = {marginHeight:'10px'};
-var imageWidth = {width:'25%'};
+var imageWidth = {width:'70px',height:'70px'};
 var imageParms = {width:'100%', height:'250px'};
+var beigeBackground = {backgroundColor:'#ffefd2', textAlign:'center'};
+var greyBackground = {backgroundColor:'#d3d3d3', textAlign:'center'};
+var colmd9 = "col-md-9";
+var clicked = false;
+var leftSize = "col-md-12";
+var rightSize = "hidden col-md-0";
+var videogame_icon = '/images/video-game.png';
+var book_icon = '/images/book-icon.png';
+var microwave_icon = '/images/microwave.png';
+var phone_icon = '/images/phone-icon.png';
+var furniture_icon = '/images/furniture.png';
+var miscellaneous_icon = '/images/box-icon.png';
+
+if(clicked)
+{
+    leftSize = "col-md-9";
+    rightSize = "col-md-3";
+}
 
 var HackPanels = React.createClass({
     render:function() {
@@ -12,47 +30,65 @@ var HackPanels = React.createClass({
     }
 })
 
-var Image = React.createClass({
-    render:function () {
+
+var Test = React.createClass({
+    render:function(){
         return(
             <div>
-            <div className="row">
-            <div className="col-sm-4">
-            <div className="thumbnail">
-                <img src={videogames_image} style={imageParms}/>
-                <div className="caption">
-                    <h2>Video Games
-                        <button type="button" className="btn btn-primary"><span className="badge">7</span> Listings</button>
-                    </h2>
-                </div>
-            </div>
-            </div>
-            <div className="col-sm-4">
-            <div className="thumbnail">
-                <img src={books_image} style={imageParms}/>
-                <div className="caption">
-                    <h2>Books
-                        <button type="button" className="btn btn-success"><span className="badge">7</span> Listings</button>
-                    </h2>
-                </div>
-            </div>
-            </div>
-
-                <div className="col-sm-4">
-                    <div className="thumbnail">
-                        <img src={appliance_image} style={imageParms}/>
-                        <div className="caption">
-                            <h2>Appliances
-                                <button type="button" className="btn btn-info"><span className="badge">7</span> Listings</button>
-                            </h2>
+                <div className="row">
+                    <div className="col-sm-4">
+                        <div className="panel panel-default" style={greyBackground}>
+                        <img src={videogame_icon} style={imageWidth}/>
+                        <h2>Video Games</h2>
+                        <button className="btn btn-info">Listings</button>
+                        </div>
+                    </div>
+                    <div className="col-sm-4">
+                        <div className="panel panel-default" style={beigeBackground}>
+                            <img src={book_icon} style={imageWidth} />
+                            <h2>Books</h2>
+                        </div>
+                    </div>
+                    <div className="col-sm-4">
+                        <div className="panel panel-default" style={greyBackground}>
+                            <img src={phone_icon} style={imageWidth} />
+                            <h2>Electronic Gadgets</h2>
                         </div>
                     </div>
                 </div>
-
-            </div>
+                <div className="row">
+                    <div className="col-sm-4">
+                        <div className="panel panel-default" style={beigeBackground}>
+                            <img src={microwave_icon} style={imageWidth}/>
+                            <h2>Home Applicances</h2>
+                        </div>
+                    </div>
+                    <div className="col-sm-4">
+                        <div className="panel panel-default" style={greyBackground}>
+                            <img src={furniture_icon} style={imageWidth} />
+                            <h2>Furnitures</h2>
+                        </div>
+                    </div>
+                    <div className="col-sm-4">
+                        <div className="panel panel-default" style={beigeBackground}>
+                            <img src={miscellaneous_icon} style={imageWidth} />
+                            <h2>Miscellanous</h2>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
 })
 
-ReactDOM.render(<Image/>,document.getElementById("test"));
+var CentralScreenLayout = React.createClass({
+    render : function() {
+        return(
+        <div>
+            <Test/>
+        </div>
+        );
+    }
+})
+
+ReactDOM.render(<Test/>,document.getElementById("test"));
